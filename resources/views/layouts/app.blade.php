d6989b43a7f25ac6cb4433337515ac2fffaad808
itsAhmed <ahmedkaid99@gmail.com>
2020-11-15 23:59:20 +0800

    Added logo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,8 +23,9 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+                <a class="navbar-brand d-flex" href="{{ url('/') }}">
+                    <div><img src="/svg/photo-camera.svg" style="height: 25px; border-right: 1px solid #333" class="pr-3"></div>
+                    <div class="pl-3 pt-1">{{ config('app.name', 'Laravel') }}</div>
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
