ee3322a56585086a0ec3c46615a18416f8f6aa6b
itsAhmed <ahmedkaid99@gmail.com>
2020-11-16 00:26:05 +0800

    Designed UI with demo data

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,21 +2,32 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
+    <div class="row">
+        <div class="col-3 p-5">
+            <img src="https://instagram.fkul15-1.fna.fbcdn.net/v/t51.2885-19/s320x320/36136463_485984398505401_8031418354793185280_n.jpg?_nc_ht=instagram.fkul15-1.fna.fbcdn.net&_nc_ohc=gquW1aRSjVgAX_y1-LM&_nc_tp=25&oh=4242395fe0060c6fc328d773624972f7&oe=5FD908E4" class="rounded-circle mw-100" alt="">
+        </div>
+        <div class="col-9 pt-5">
+            <div><h1>digimonqq</h1></div>
+            <div class="d-flex">
+                <div class="pr-5"><strong>0</strong> posts</div>
+                <div class="pr-5"><strong>192</strong> followers</div>
+                <div class="pr-5"><strong>187</strong> following</div>
             </div>
+            <div class="pt-4 font-weight-bold">Ahmed</div>
+            <div>Testy test test is testing a testly test.</div>
+            <div><a href="#">ahmedme.tech</a></div>
+        </div>
+    </div>    
+
+    <div class="row pt-5">
+        <div class="col-4">
+            <img src="https://instagram.fkul15-1.fna.fbcdn.net/v/t51.2885-19/s320x320/36136463_485984398505401_8031418354793185280_n.jpg?_nc_ht=instagram.fkul15-1.fna.fbcdn.net&_nc_ohc=gquW1aRSjVgAX_y1-LM&_nc_tp=25&oh=4242395fe0060c6fc328d773624972f7&oe=5FD908E4" class="w-100">
+        </div>
+        <div class="col-4">
+            <img src="https://instagram.fkul15-1.fna.fbcdn.net/v/t51.2885-19/s320x320/36136463_485984398505401_8031418354793185280_n.jpg?_nc_ht=instagram.fkul15-1.fna.fbcdn.net&_nc_ohc=gquW1aRSjVgAX_y1-LM&_nc_tp=25&oh=4242395fe0060c6fc328d773624972f7&oe=5FD908E4" class="w-100">
+        </div>
+        <div class="col-4">
+            <img src="https://instagram.fkul15-1.fna.fbcdn.net/v/t51.2885-19/s320x320/36136463_485984398505401_8031418354793185280_n.jpg?_nc_ht=instagram.fkul15-1.fna.fbcdn.net&_nc_ohc=gquW1aRSjVgAX_y1-LM&_nc_tp=25&oh=4242395fe0060c6fc328d773624972f7&oe=5FD908E4" class="w-100">
         </div>
     </div>
 </div>
