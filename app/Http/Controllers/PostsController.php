e68e693d555a70b49721ac9c9060d9ce4bd81b43
itsAhmed <ahmedkaid99@gmail.com>
2020-11-17 00:12:26 +0800

    Image Resize Feature

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Post;
 use Illuminate\Http\Request;
+use Intervention\Image\Facades\Image;
 
 class PostsController extends Controller
 {
@@ -26,6 +27,9 @@ class PostsController extends Controller
         ]);
 
         $image_path  = request('image')->store('uploads', 'public');
+
+        $image = Image::make(public_path("storage/{$image_path}"))->fit(1200, 1200);
+        $image->save();
 
         auth()->user()->posts()->create([
             'caption' => $data['caption'],
