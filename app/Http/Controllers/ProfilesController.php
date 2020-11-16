0dfa96077cc70623a87ea408c2628b4ee2c5dda6
itsAhmed <ahmedkaid99@gmail.com>
2020-11-16 22:15:18 +0800

    Added Profile mmcv and user tinker a lot

---
+++ app/Http/Controllers/ProfilesController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+use Illuminate\Http\Request;
+
+class ProfilesController extends Controller
+{
+    public function index($user)
+    {
+        $user = User::find($user);
+        return view('home', [
+            'user' => $user,
+        ]);
+    }
+}
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,15 +7,15 @@
             <img src="https://instagram.fkul15-1.fna.fbcdn.net/v/t51.2885-19/s320x320/36136463_485984398505401_8031418354793185280_n.jpg?_nc_ht=instagram.fkul15-1.fna.fbcdn.net&_nc_ohc=gquW1aRSjVgAX_y1-LM&_nc_tp=25&oh=4242395fe0060c6fc328d773624972f7&oe=5FD908E4" class="rounded-circle mw-100" alt="">
         </div>
         <div class="col-9 pt-5">
-            <div><h1>digimonqq</h1></div>
+            <div><h1>{{ $user->username }}</h1></div>
             <div class="d-flex">
                 <div class="pr-5"><strong>0</strong> posts</div>
                 <div class="pr-5"><strong>192</strong> followers</div>
                 <div class="pr-5"><strong>187</strong> following</div>
             </div>
-            <div class="pt-4 font-weight-bold">Ahmed</div>
-            <div>Testy test test is testing a testly test.</div>
-            <div><a href="#">ahmedme.tech</a></div>
+            <div class="pt-4 font-weight-bold">{{ $user->profile->title }}</div>
+            <div>{{ $user->profile->description ?? 'N/A' }}</div>
+            <div><a href="{{ $user->profile->url }}">{{ $user->profile->url }}</a></div>
         </div>
     </div>    
 
